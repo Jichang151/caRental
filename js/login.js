3eac6debb81257e27361efc5bae49f7b31b05cd1
1 <www.602205405@qq.com>
2018-03-18 12:59:06 +0800

    登录未完待续。。。

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
 /**
  * Created by 1 on 2018/3/17.
  */
+(function(){
+    var oContent = document.getElementById("content");
+    var oUserName = oContent.getElementById("username");
+    var oSubmit = oContent.getElementById('submit');
+    oSubmit.onclick = function(){
+        if(oUserName.value = '' ){
+            alert('用户名不能为空');
+        }else{
+            alert('asd');
+        }
+
+    };
+});
