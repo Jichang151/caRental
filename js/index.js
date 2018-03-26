9bc6b54225b45aca6a8c4509fbaa1e866586f16d
1 <www.602205405@qq.com>
2018-03-26 22:23:12 +0800

    个人中心未完待续。。。

--- js/index.js
+++ js/index.js
@@ -8,6 +8,33 @@ window.onload = function(){
         var aCenterNav = aMainNav[0].getElementsByClassName('center_navigation');
         var aUlNav = aCenterNav[0].getElementsByTagName('ul');
         var aLiNav = aUlNav[0].getElementsByTagName('li');
-        console.log(aLiNav);
+
+        var oContent = document.getElementById('content');
+        var aPicture = oContent.getElementsByClassName('picture');
+        var aConImg = aPicture[0].getElementsByTagName('img');
+        //console.log(aPicture);
+        var aConUl = oContent.getElementsByClassName('row');
+        //console.log(aConUl);
+        var aConLi = aConUl[0].getElementsByTagName('li');
+        //console.log(aConLi);
+        for(var i=0;i<aConLi.length;i++){
+            aConLi[i].onmouseover = function(){
+                for(var i=0;i<aConLi.length;i++){
+                    aConLi[i].className = '';
+                }
+                this.className = 'selected';
+            }
+        }
+
+        function changeImg(){
+
+            //var aPicture = oContent.getElementsByClassName('picture');
+            //console.log(aLiNav);
+
+            for(var i=0;i<aConImg.length;i++){
+
+            }
+
+        }
     })();
 };
